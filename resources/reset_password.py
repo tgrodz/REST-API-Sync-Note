d8094aed314ec278d5cb90462eaa5d0e3674c397
tgrodz <tgrodzichkiy@gmail.com>
2021-05-10 10:16:19 +0300

    first commit

---
+++ resources/reset_password.py
@@ -0,0 +1,97 @@
+from flask import request, render_template
+from flask_jwt_extended import create_access_token, decode_token
+from database.models import User
+from flask_restful import Resource
+import datetime
+from resources.errors import SchemaValidationError, InternalServerError, \
+    EmailDoesnotExistsError, BadTokenError
+from jwt.exceptions import ExpiredSignatureError, DecodeError, \
+    InvalidTokenError
+
+from services.mail_service_gmail import send_email
+
+# Metaclass
+class Singleton(type):
+    """ Define an instance operation that lets client access it's unique instance """
+
+    def __init__(cls, name, bases, attrs, **kwargs):
+        super().__init__(name, bases, attrs)
+        cls._instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__call__(*args, **kwargs)
+        return cls._instance
+
+
+class LinkToken(metaclass=Singleton):
+    """ Link  """
+    expires = datetime.timedelta(hours=24)
+    reset_token = ''
+    pass
+
+
+class ForgotPassword(Resource):
+    def post(self):
+        url = request.host_url + 'reset/'
+        try:
+            body = request.get_json()
+            email = body.get('email')
+            if not email:
+                raise SchemaValidationError
+
+            user = User.objects.get(email=email)
+            if not user:
+                raise EmailDoesnotExistsError
+
+            link_token = LinkToken()
+            link_token.reset_token = create_access_token(str(user.id), expires_delta=link_token.expires)
+
+            return send_email('[Sync-note] Reset Your Password',
+                              sender='support@sync-note.com',
+                              recipients=[user.email],
+                              text_body=render_template('email/reset_password.txt',
+                                                        url=url + link_token.reset_token),
+                              html_body=render_template('email/reset_password.html',
+                                                        url=url + link_token.reset_token))
+        except SchemaValidationError:
+            raise SchemaValidationError
+        except EmailDoesnotExistsError:
+            raise EmailDoesnotExistsError
+        except Exception as e:
+            raise InternalServerError
+
+
+class ResetPassword(Resource):
+    def post(self):
+        url = request.host_url + 'reset/'
+        try:
+            body = request.get_json()
+            reset_token = body.get('reset_token')
+            password = body.get('password')
+
+            if not reset_token or not password:
+                raise SchemaValidationError
+
+            user_id = decode_token(reset_token)['identity']
+
+            user = User.objects.get(id=user_id)
+
+            user.modify(password=password)
+            user.hash_password()
+            user.save()
+
+            return send_email('[Sync-note] Password reset successful',
+                              sender='support@sync-note.com',
+                              recipients=[user.email],
+                              text_body='Password reset was successful',
+                              html_body='<p>Password reset was successful</p>')
+
+        except SchemaValidationError:
+            raise SchemaValidationError
+        except ExpiredSignatureError:
+            raise ExpiredTokenError
+        except (DecodeError, InvalidTokenError):
+            raise BadTokenError
+        except Exception as e:
+            raise InternalServerError
